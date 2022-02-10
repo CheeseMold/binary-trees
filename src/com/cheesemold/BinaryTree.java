4231c3ea0f90323d6c23bac363cf9ce9dfe664c0
Henrique Moreira <henriquebear17@gmail.com>
2022-02-10 15:23:52 -0300

    Added traverse tree in pre order method and remove node method

--- src/com/cheesemold/BinaryTree.java
+++ src/com/cheesemold/BinaryTree.java
@@ -4,12 +4,19 @@ package com.cheesemold;
 public class BinaryTree {
     private Node root;
 
+    public Node getRoot() {
+        return root;
+    }
+
     public void addNode(int key, String name) {
         Node newNode = new Node(key, name);
         
+        // Sets root to the new node if it does not yet exist
         if (root == null) {
             root = newNode;
         } else {
+            // Sets default value of current node being analised as 'root', and initializes parent Object for 
+            // the parent node of focusNode
             Node focusNode = root;
             Node parent;
 
@@ -31,5 +38,51 @@ public class BinaryTree {
             }
         }
     }
+    public void addNode(Node addNode) {
+        addNode(addNode.key, addNode.name);
+    }
 
+    // Traverses through all nodes in tree 'in order'. In other words, start from the smallest value in the 
+    // tree.
+    public void traverseTreeInOrder(Node focusNode) {
+        if (focusNode != null) {
+            // Uses recursion to traverse, or iterate through each Binary Tree element
+            traverseTreeInOrder(focusNode.leftChild);
+            System.out.println(focusNode);
+            traverseTreeInOrder(focusNode.rightChild);
+        }
+    }
+
+    public void traverseTreePreOrder(Node focusNode) {
+        if (focusNode != null) {
+            System.out.println(focusNode);
+            traverseTreeInOrder(focusNode.leftChild);
+            traverseTreeInOrder(focusNode.rightChild);
+        }
+    }
+
+    public void removeNode(Node removeNode) {
+        Node focusNode = root;
+        Node parent;
+        if (removeNode.key == focusNode.key) {
+            root = null;
+        } else {
+            while (focusNode != null) {
+                parent = focusNode;
+                if (removeNode.key < focusNode.key) {
+                    focusNode = focusNode.leftChild;
+                    if (focusNode != null && focusNode.key == removeNode.key) {
+                        parent.leftChild = null;
+                        return;
+                    }
+                } else {
+                    focusNode = focusNode.rightChild;
+                    if (focusNode != null && focusNode.key == removeNode.key) {
+                        parent.rightChild = null;
+                        return;
+                    }
+                }    
+            }
+        }
+    }
 }
--- src/com/cheesemold/Main.java
+++ src/com/cheesemold/Main.java
@@ -2,8 +2,26 @@ package com.cheesemold;
 
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
+
+        BinaryTree myBinaryTree = new BinaryTree();
+        Node last = new Node(110, "n8");
+        myBinaryTree.addNode(50, "n1");
+        myBinaryTree.addNode(30, "n2");
+        myBinaryTree.addNode(70, "n3");
+        myBinaryTree.addNode(10, "n4");
+        myBinaryTree.addNode(50, "n5");
+        myBinaryTree.addNode(90, "n6");
+        myBinaryTree.addNode(70, "n7");
+        myBinaryTree.addNode(last);
+
+        //myBinaryTree.traverseTreeInOrder(myBinaryTree.getRoot());
+
+        myBinaryTree.traverseTreePreOrder(myBinaryTree.getRoot());
+
+        myBinaryTree.removeNode(new Node(110, "343"));
+        System.out.println("\n");
+        myBinaryTree.traverseTreePreOrder(myBinaryTree.getRoot());
     }
 
 }
